dcdf4a6e453bc1798354663e97a0284aa2a8920f
kafkaesc <jared.a.hettinger@gmail.com>
2023-12-18 18:30:10 -0600

    Adding H1.test.tsx

---
+++ app/elements/H1.test.tsx
@@ -0,0 +1,13 @@
+// TODO: Get Jest and JSTL working
+/*
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import H1 from './H1';
+
+it('Renders an h1 Hello World', () => {
+	render(<H1>Hello World</H1>);
+	const heading = screen.getByRole('heading', { level: 1 });
+	expect(heading).toBeInTheDocument();
+	expect(heading).toHaveTextContent(/hello world/i);
+});
+*/
