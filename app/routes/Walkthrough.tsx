71759b333b354f58990f11df26d8321ff970ba34
kafkaesc <jared.a.hettinger@gmail.com>
2023-12-21 14:26:43 -0600

    Updating the Walkthrough page

--- app/routes/Walkthrough.tsx
+++ app/routes/Walkthrough.tsx
@@ -1,9 +1,23 @@
+import H1 from '~/elements/H1';
+import Link from '~/elements/Link';
 import WalkthroughBody from '~/components/WalkthroughBody';
 
 export default function Walkthrough() {
 	return (
 		<div>
-			<WalkthroughBody />
+			<H1>A Walkthrough of the Roll Commands</H1>
+			<div className="text-center">
+				<Link to="/Room" className="mx-1">
+					Chatroom
+				</Link>
+				<Link to="/Walkthrough" className="mx-1">
+					Walkthrough
+				</Link>
+				<Link to="/About" className="mx-1">
+					About
+				</Link>
+			</div>
+			<WalkthroughBody hideTopHeading={true} />
 		</div>
 	);
 }
