1dcdc3bd3b93350336f69cbc9e2ddcb3187be076
kafkaesc <jared.a.hettinger@gmail.com>
2023-12-21 10:49:03 -0600

    Adding Link element component

---
+++ app/elements/Link.tsx
@@ -0,0 +1,29 @@
+import { Link as ReLink } from '@remix-run/react';
+
+interface LinkProps {
+	children: any;
+	className?: string;
+	to: string;
+	[props: string]: any;
+}
+
+export default function Link({
+	children,
+	className,
+	props,
+	to,
+}: LinkProps): JSX.Element {
+	return className ? (
+		<ReLink
+			{...props}
+			to={to}
+			className={`text-blue-600 hover:underline ${className}`}
+		>
+			{children}
+		</ReLink>
+	) : (
+		<ReLink {...props} to={to} className="text-blue-600 hover:underline">
+			{children}
+		</ReLink>
+	);
+}
